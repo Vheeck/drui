ab6ae5b1decae1e7445148eb073dae8c44d65715
Vheeck <momohvictor62@gmail.com>
2020-02-13 01:21:31 +0100

    Done with page 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,21 @@
 let $ = (selector) => document.querySelector(selector);
 let _ = (selector) => document.querySelectorAll(selector);
 
-let dropdowns = _('.dropdown-content')
+let dropdowns = _('.dropdown-content');
+let activeDropdown = _('.dropdown.active');
 
 let activateDropdown = () => {
 
     if (dropdowns) {
         dropdowns.forEach(dropdown => {
             dropdown.parentElement.addEventListener('click', () => {
-                if (dropdown.parentElement.dataset.toggled == 'false') {
-                    dropdown.parentElement.dataset.toggled = 'true';
-                    dropdown.classList.add('toggled');
-                }
-                else {
+                if (dropdown.parentElement.dataset.toggled == 'true') {
                     dropdown.parentElement.dataset.toggled = 'false';
                     dropdown.classList.remove('toggled');
+                }
+                else {
+                    dropdown.parentElement.dataset.toggled = 'true';
+                    dropdown.classList.add('toggled');
                 }
             });
         });
@@ -23,3 +24,4 @@ let activateDropdown = () => {
 }
 
 activateDropdown();
+activeDropdown[0].click();
